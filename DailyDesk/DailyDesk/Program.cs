bf6f405ed65c690bc3d8ab475357c540b66f4387
Gillvano <freegilioshouse@gmail.com>
2024-10-08 11:58:48 +0200

    Services + Connection String + Project Reference

--- DailyDesk/DailyDesk/Program.cs
+++ DailyDesk/DailyDesk/Program.cs
@@ -1,10 +1,18 @@
+
+
 namespace DailyDesk
 {
+    using DataAccess.DB;
+
     public class Program
     {
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // Register DatabaseConnection
+            builder.Services.AddSingleton<DatabaseConnection>(_ =>
+            new DatabaseConnection("Server=mssqlstud.fhict.local;Database=dbi507117_dailydesk;User Id=dbi507117_dailydesk;Password=pl@crC7ai#;"));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
--- DailyDesk/DataAccess/DB/DatabaseConnection.cs
+++ DailyDesk/DataAccess/DB/DatabaseConnection.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 
 namespace DataAccess.DB
 {
     public class DatabaseConnection
     {
+        public readonly string connectionString;
+        public DatabaseConnection(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void StartConnection(Action<DbConnection> action)
+        {
+            using (DbConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                action.Invoke(connection);
+                connection.Close();
+            }
+        }
     }
 }
